45cde67e2b9124fa3cf3c9b0f04179a81330c1b0
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-25 07:19:44 -0400

    working on tinderCards

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -10,18 +10,34 @@ function TinderCards() {
 
     const [people, setPeople] = useState([
 
-        { name: "eeee", mmm: "mmmm" },
+        { name: "eeee", im: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/11.10.2015_Samoyed_%28cropped%29.jpg/220px-11.10.2015_Samoyed_%28cropped%29.jpg" },
+        { name: "aaa", im: "https://cdn.pixabay.com/photo/2018/05/30/00/24/thunderstorm-3440450_960_720.jpg" },
+
     ]);
 
+    const swiped = (direction) => {
+        console.log('You swiped: ' + direction)
+      }
+      
+      const onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+      }
     return (
         <div className="tindercards">
             <div className="tindercardsContainer">
                 {people.map(person => (
-                    <TinderCard onSwipe={onSwipe}
-                        onCardLeftScreen={() => onCardLeftScreen('fooBar')}
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        onSwipe={dir => swiped(dir, person.name)}
+                        onCardLeftScreen={() => onCardLeftScreen(person)}
                         preventSwipe={['right', 'left']}
                     >
-                        Hello, World! </TinderCard>
+                        <div style={{backgroundImage:`url(${person.im})`}}>
+                           <h3>{person.name}</h3> 
+                        </div>
+                        
+                         </TinderCard>
 
                 ))}
             </div>
