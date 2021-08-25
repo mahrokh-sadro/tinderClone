62a5a9545bdabc277c0fe208cc7aee83ab78b5d6
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-25 04:35:05 -0400

    working on header

--- src/Header.js
+++ src/Header.js
@@ -9,6 +9,12 @@ function Header() {
             <IconButton>
                 <PersonIcon fontSize="large" className="header_icon" />
             </IconButton>
+            <img
+                className="header_logo"
+                src="http://america.aljazeera.com/content/ajam/blogs/scrutineer/2014/7/2/tinder-cofoundertakesswipeatdatingappssexistworkplace/jcr:content/blog/mainpar/textimage/image.adapt.990.high.Tinder_logo_070214.1404740996049.jpg"
+
+            />
+
         </div>
     )
 }
