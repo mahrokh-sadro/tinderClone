c3010dd48bf2e870b9bfc9ff464aebf69bbeb02f
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-25 03:35:36 -0400

    makinh header and dl materila ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="app">
+    <h1>mern app</h1>
+    <Header />
+    {/* {cards} */}
+    {/* {buttons} */}
     </div>
   );
 }
