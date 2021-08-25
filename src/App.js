288991a98269cc34075893c904e72ae0308a1c59
AutoLogon <msadrolodabaee@myseneca.ca>
2021-08-25 10:14:58 -0400

    some buttombuttons.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import TinderCards from "./TinderCards";
+import ButtomButtons from './ButtomButtons';
 
 function App() {
   return (
--- src/ButtomButtons.js
+++ src/ButtomButtons.js
@@ -1,13 +1,29 @@
 
 
 import React from 'react'
-import "./ButtomButtons"
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import StarIcon from '@material-ui/icons/Star';
+import IconButton from '@material-ui/core/IconButton';
+import "./ButtomButtons.css"
 
 
 function ButtomButtons() {
     return (
-        <div>
-            
+        <div className="bottomButton">
+            <IconButton>
+                <ReplayIcon fontSize="large"/>
+            </IconButton>
+            <IconButton>
+                <CloseIcon fontSize="large"/>
+            </IconButton>
+            <IconButton>
+                <FavoriteIcon fontSize="large"/>
+            </IconButton>
+            <IconButton>
+                <StarIcon fontSize="large"/>
+            </IconButton>
         </div>
     )
 }
